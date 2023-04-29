205425287986153b1862f2f562ad708d488e8b1d
sinus22 <yusupov.giyos@gmail.com>
2023-04-29 14:08:46 +0500

    login page change

--- userApp/urls.py
+++ userApp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("login", views.login_view)
+    path("login", views.user_login, name='user_login'),
+    path("logout", views.user_logout, name='user_logout')
 ]
--- userApp/views.py
+++ userApp/views.py
@@ -1,24 +1,23 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpRequest
 from django.shortcuts import render, redirect
 
 
 # Create your views here.
 
-def login_view(req: HttpRequest):
-    print(req.user)
+def user_login(req: HttpRequest):
     if req.method == 'POST':
         username = req.POST['username']
         password = req.POST['password']
-        print(username)
-        print(password)
 
         user = authenticate(req, username=username, password=password)
-        login(req, user)
-
-        print(user)
-        # if user is not None:
-        #     login(req, user)
-        #     return redirect(home_index)
-
+        if user is not None:
+            print(user)
+            login(req, user)
+            return redirect("patient_list")
     return render(req, "login.html")
+
+
+def user_logout(req: HttpRequest):
+    logout(req)
+    return redirect("user_login")
