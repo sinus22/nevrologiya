7687b847623081a0182c282013cd4733eee8acb2
sinus22 <yusupov.giyos@gmail.com>
2023-05-04 10:37:19 +0500

    add run app

--- runapp.bat
+++ runapp.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 FOR /F "delims=: tokens=2" %%a in ('ipconfig ^| find "IPv4"') do set _IPAddress=%%a
-cd /d %~dp0 & "venv\Scripts\activate" & python manage.py runserver %_IPAddress%:80
+cd /d %~dp0 & "venv\Scripts\activate" & python manage.py runserver %_IPAddress%:8000
 cmd /k
